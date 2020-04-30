97d955b08c1be7c554b3a9163976c4cc659257f5
crerar <crerar@crerarui-MacBookPro.local>
2020-04-30 14:42:53 +0900

    Updates

---
+++ precache-manifest.2c0f4ab199a6ff7d063bf3a14245dbca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0304e8ff2dce41ba1f06c686a59273d2",
+    "url": "/kimyoungjae96/portfolio/index.html"
+  },
+  {
+    "revision": "0c8c9e028820093257b0",
+    "url": "/kimyoungjae96/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "afda21390b6b4cb282a8",
+    "url": "/kimyoungjae96/portfolio/static/js/2.262eeec0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kimyoungjae96/portfolio/static/js/2.262eeec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c8c9e028820093257b0",
+    "url": "/kimyoungjae96/portfolio/static/js/main.5fa0242e.chunk.js"
+  },
+  {
+    "revision": "97d89c577857185fd68c",
+    "url": "/kimyoungjae96/portfolio/static/js/runtime-main.0cd57dd9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kimyoungjae96/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
